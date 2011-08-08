28a1430c5424477654d513ca4ccc4be123305324
Stepan Generalov <psi.pk.ru@gmail.com>
2011-08-08 18:29:59 +0400

    Cleanup comments and documentation. Closes #37

--- Extensions/CCMenuItemSpriteIndependent/CCMenuItemSpriteIndependent.h
+++ Extensions/CCMenuItemSpriteIndependent/CCMenuItemSpriteIndependent.h
@@ -33,7 +33,7 @@
  * and disabled images as children. Instead of that its just retain them.
  * So you can place images anyhow you want.
  *
- * CCmenuItemSprite reimplements rect and convertToNodeSpace: methods
+ * CCMenuItemSpriteIndependent reimplements rect and convertToNodeSpace: methods
  * delegating them to normalSprite.
  * This allows you to position/scale/rotate only normal sprite and forget about
  * positioning menuItem.
