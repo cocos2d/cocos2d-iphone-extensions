95af00aa8f1b1101e8b1af18d8b287a327b39589
Alexey Lang <a.lang@pixonic.ru>
2011-10-18 16:43:17 +0400

    CCLayerPanZoomTest: change test params

--- Tests/CCLayerPanZoomTest/CCLayerPanZoomTestLayer.m
+++ Tests/CCLayerPanZoomTest/CCLayerPanZoomTestLayer.m
@@ -256,8 +256,8 @@ Class backTest()
                               z: 1 
                             tag: kLabelTag];
         _panZoomLayer.mode = kCCLayerPanZoomModeSheet;
-        _panZoomLayer.ruberEdgesMargin = 150.0f;;
-        _panZoomLayer.ruberEdgesTime = 0.1f;
+        _panZoomLayer.ruberEdgesMargin = [CCDirector sharedDirector].winSize.width / 2.0;
+        _panZoomLayer.ruberEdgesTime = 0.2f;
 		[self updateForScreenReshape];
 	}
 	
